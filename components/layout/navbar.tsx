9c26513c327ad8e37486e3a6d068dfbb18d4d08e
Leonardo <leonardo.placanica@gmail.com>
2024-06-19 12:07:10 +0200

    feat: added style

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -53,7 +53,7 @@ export function NavBar({ scroll = false }: NavBarProps) {
         <div className="flex gap-6 md:gap-10">
           <Link href="/" className="flex items-center space-x-2">
             <Icons.logo />
-            <span className="font-urban text-xl font-bold">
+            <span className="text-gradient_theme_colors font-urban text-xl font-bold">
               {siteConfig.name}
             </span>
           </Link>
